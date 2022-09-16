dd94c3392947f7c6947b85aea8eb1e13cdacf45c
Nick Clifford <nick@nickclifford.me>
2022-09-15 20:41:53 -0500

    build_rule_parsers

--- smolasm/src/parse/mod.rs
+++ smolasm/src/parse/mod.rs
@@ -8,34 +8,57 @@ mod ast;
 #[grammar = "parse/assembly.pest"]
 struct AssemblyParser;
 
-fn ident(input: Pair<Rule>) -> ast::Ident {
-    assert_eq!(input.as_rule(), Rule::ident);
-    input.as_str().to_string()
-}
-fn dotted(input: Pair<Rule>) -> ast::Dotted {
-    assert_eq!(input.as_rule(), Rule::dotted);
-    ast::Dotted(input.into_inner().map(ident).collect())
+macro_rules! build_rule_parsers {
+    ($( $rule:ident($input:ident) -> $t:ty { $($body:tt)* } )*) => {
+        $(
+            fn $rule($input: Pair<Rule>) -> $t {
+                assert_eq!($input.as_rule(), Rule::$rule);
+                $($body)*
+            }
+        )*
+    };
 }
 
-fn asm_spec(input: Pair<Rule>) -> ast::AssemblySpec {
-    assert_eq!(input.as_rule(), Rule::asm_spec);
-    let mut inner = input.into_inner();
-    ast::AssemblySpec {
-        assembly: dotted(inner.next().unwrap()),
-        version: inner.next().map(|v| {
-            ast::Version(
-                v.as_str()[1..]
-                    .split('.')
-                    .map(|n| n.parse().unwrap())
-                    .collect(),
-            )
-        }),
+build_rule_parsers! {
+    ident(input) -> ast::Ident {
+        input.as_str().to_string()
     }
-}
+    dotted(input) -> ast::Dotted {
+        ast::Dotted(input.into_inner().map(ident).collect())
+    }
+    asm_spec(input) -> ast::AssemblySpec {
+        let mut inner = input.into_inner();
+        ast::AssemblySpec {
+            assembly: dotted(inner.next().unwrap()),
+            version: inner.next().map(|v| {
+                ast::Version(
+                    v.as_str()[1..]
+                        .split('.')
+                        .map(|n| n.parse().unwrap())
+                        .collect(),
+                )
+            }),
+        }
+    }
+    enum_decl(input) -> ast::Enum {
+        todo!()
+    }
+    type_decl(input) -> ast::TypeDeclaration {
+        todo!()
+    }
+    top_level_decl(input) -> ast::TopLevel {
+        let public = input.as_str().starts_with("public");
+        let inner = input.into_inner().next().unwrap();
 
-fn top_level_decl(input: Pair<Rule>) -> ast::TopLevel {
-    assert_eq!(input.as_rule(), Rule::top_level_decl);
-    todo!()
+        ast::TopLevel {
+            public,
+            kind: match inner.as_rule() {
+                Rule::enum_decl => ast::TopLevelKind::Enum(enum_decl(inner)),
+                Rule::type_decl => ast::TopLevelKind::Type(type_decl(inner)),
+                _ => unreachable!(),
+            },
+        }
+    }
 }
 
 pub fn assembly(input: &str) -> Result<ast::Assembly, pest::error::Error<Rule>> {
@@ -48,12 +71,13 @@ pub fn assembly(input: &str) -> Result<ast::Assembly, pest::error::Error<Rule>> 
     for pair in parse {
         match pair.as_rule() {
             Rule::extern_decl => {
-                let mut inner = e.into_inner();
+                let mut inner = pair.into_inner();
                 extern_decls.push(asm_spec(inner.next().unwrap()));
             }
             Rule::top_level_decl => {
                 top_level_decls.push(top_level_decl(pair));
             }
+            _ => unreachable!(),
         }
     }
 
